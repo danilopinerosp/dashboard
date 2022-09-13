5fe7ae44aacfd6124e4887f47ebcc8e4656f06d1
Danilo Piñeros <dspinerosp@gmail.com>
2022-09-12 21:02:23 -0500

    Format src/pages/app.py

--- src/pages/app.py
+++ src/pages/app.py
@@ -7,10 +7,7 @@ from features.ui.layout.index import layout
 
 server = flask.Flask(__name__)
 
-external_script = [
-    "https://tailwindcss.com/", 
-    {"src": "https://cdn.tailwindcss.com"}
-]
+external_script = ["https://tailwindcss.com/", {"src": "https://cdn.tailwindcss.com"}]
 
 app = dash.Dash(
     __name__,
@@ -18,7 +15,7 @@ app = dash.Dash(
     suppress_callback_exceptions=True,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
     title="Dashboard App",
-    external_scripts=external_script
+    external_scripts=external_script,
 )
 
 app.scripts.config.serve_locally = True
