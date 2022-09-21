d21d6c4e047eed1f45d965653408eb01cd7f2791
Danilo Piñeros <dspinerosp@gmail.com>
2022-09-21 17:32:36 -0500

    Create prototype for overview view

--- production_dashboard/views.py
+++ production_dashboard/views.py
@@ -1,4 +1,9 @@
 """Production dashboard views."""
 # from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def overview():
+    """View for overview page."""
+    return HttpResponse("Index view.")
